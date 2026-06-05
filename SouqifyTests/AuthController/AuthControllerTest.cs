a94972559f1f7fa2e9be07d6482c58bbb3978fdd
Fahed <fahedalobaidi1999@gmail.com>
2026-06-05 20:25:50 +0300

    adding unit test for login and build mocking for it

--- SouqifyTests/AuthController/AuthControllerTest.cs
+++ SouqifyTests/AuthController/AuthControllerTest.cs
@@ -2,8 +2,13 @@
 
 using Bogus;
 using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Souqify.Application.DTOs.Auth;
+using Souqify.Infrastructure;
+using Souqify.Infrastructure.Identity;
 using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
 
 namespace SouqifyTests.AuthController
 {
@@ -43,5 +48,64 @@ namespace SouqifyTests.AuthController
             createdUser.AccessToken.Should().NotBeNullOrEmpty();
             createdUser.RefreshToken.Should().NotBeNullOrEmpty();
         }
+
+        [Fact]
+        public async Task LoginAsync_ReturnOk()
+        {
+            var faker = new Faker();
+
+            var fakeEmail = faker.Person.Email;
+
+            var applicationUser = new ApplicationUser
+            {
+                Email = fakeEmail,
+                UserName= fakeEmail,
+                FirstName = faker.Person.FirstName,
+                LastName = faker.Person.LastName,
+                PhoneNumber = "1112223332"
+            };
+
+            using var scope = _souqifyApiFactory.Services.CreateScope();
+
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            var userManagerResult= await userManager.CreateAsync(applicationUser, "Sa123456");
+            if (!userManagerResult.Succeeded)
+            {
+                var errors = string.Join("; ", userManagerResult.Errors.Select(e => e.Description));
+
+                throw new Exception($"Seed failed:{errors}");
+            }
+
+            var roleResult= await userManager.AddToRoleAsync(applicationUser, "Customer");
+
+            if (!roleResult.Succeeded)
+            {
+                var errors = string.Join("; ", roleResult.Errors.Select(e => e.Description));
+
+                throw new Exception($"Seed failed:{errors}");
+            }
+
+            var loginDto = new LoginRequestDto
+            {
+                Email = fakeEmail,
+                Password = "Sa123456"
+            };
+
+            var response = await _httpClient.PostAsJsonAsync("api/auth/login", loginDto);
+
+            var result = await response.Content.ReadFromJsonAsync<AuthResponseDto>();
+
+            result.Should().NotBeNull();
+            result.AccessToken.Should().NotBeNullOrEmpty();
+            result.RefreshToken.Should().NotBeNullOrEmpty();
+
+        }
+
+        //[Fact]
+        //public async Task Refresh_returnOk()
+        //{
+
+        //}
     }
 }
